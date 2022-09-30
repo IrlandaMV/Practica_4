60218c5174e9750d26a607347ccee0bfb13e3172
Irlanda Maldonado <irly_malvel@hotmail.com>
2022-09-29 18:44:35 -0700

    Actualizacion

--- Practica_4/Program.cs
+++ Practica_4/Program.cs
@@ -6,7 +6,16 @@ namespace Practica_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            double c,f;
+
+            Console.WriteLine("Dame los grados centigrados");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            f = (c*1.8) + 32;
+
+            Console.WriteLine("Los grados Farenheit son:{0}",f);
+
         }
     }
 }
